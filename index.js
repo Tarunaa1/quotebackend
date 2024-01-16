6c91f1e03141a5553058ac07bb7da6cc79211a24
Tarunaa1 <140168096+Tarunaa1@users.noreply.github.com>
2024-01-16 12:25:56 +0530

    quoteapi

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const  mongoclient = require("mongodb").MongoClient;
+const cors = require("cors");
+const multer = require("multer");
+
+
+const app = express();
+app.use(cors());
+
+const username = "taruna";
+const password = "Taruna@01";
+const dbName = "randomquote";
+
+// const CONNECTION_STRING = `mongodb+srv://${encodeURIComponent(username)}:${encodeURIComponent(password)}@cluster0.wlyuys2.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+const CONNECTION_STRING =  `mongodb+srv://${encodeURIComponent(username)}:${encodeURIComponent(password)}@cluster0.wlyuys2.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+// const client = new mongoclient(CONNECTION_STRING);
+
+let database ; 
+
+app.listen(5000, ()=>{
+    const client = new mongoclient(CONNECTION_STRING);
+    database = client.db(dbName);
+    console.log("Mongodb connected succesfully!");
+    }
+)
+app.get("/test",(req,res)=>{
+    res.send("test route working");
+})
+
+
+app.get("/quotes", async (req, res) => {
+    try {
+        const quotes = await database.collection("quotes").find({}).toArray();
+        console.log('Quotes retrieved successfully:');
+        res.send(quotes);
+    } catch (error) {
+        console.error("Error retrieving quotes:", error);
+        res.status(500).send("Internal Server Error");
+    }
+})
+
+
+app.post("/addquote", multer().none(), (req,res)=>{
+    database.collection("quotes").count({},function(err,numOfDocs){
+        database.collection("quotes").insertOne({
+            id:(numOfDocs+1).toString(),
+            quote : req.body.newQuote,
+            author : req.body.name
+        });
+        res.json("Added Successfully");
+    })
+})
